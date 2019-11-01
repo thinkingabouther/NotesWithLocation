b9752400fc009f59c21ad0b408e8cab7c33af326
Arseny Neustroev <neustroev.arseny@gmail.com>
2019-11-01 23:17:22 +0500

    Initial Commit

---
+++ NotesWithLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesWithLocation
+//
+//  Created by Arseny Neustroev on 01/11/2019.
+//  Copyright © 2019 Arseny Neustroev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
